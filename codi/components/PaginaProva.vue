b6c13f87fbf4c8e24686bfec7ee50cbbb1971b6d
JordiCar-design <jordicll@gmail.com>
2023-01-12 17:01:10 +0100

    1er commit

---
+++ codi/components/PaginaProva.vue
@@ -0,0 +1,12 @@
+<!-- <template> 
+    <h1>
+        Text en construcció
+    </h1>
+    <img src="https://cdn.pixabay.com/photo/2022/11/28/14/07/skyline-7622147_960_720.jpg">
+</template>
+
+<script>
+export default{
+
+}
+</script> -->
---
+++ codi/pages/prova.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>
+        Text en construcció
+    </h1>
+
+    <img src="https://cdn.pixabay.com/photo/2022/11/28/14/07/skyline-7622147_960_720.jpg">
+
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+        image: require('~/https://cdn.pixabay.com/photo/2022/11/28/14/07/skyline-7622147_960_720.jpg')
+        }
+    }
+}
+</script>
